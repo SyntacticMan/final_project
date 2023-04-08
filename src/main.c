34c55dcc4003f9501388e1da61654e58af43ab48
Nuno Méren <nuno.meren@gmail.com>
2023-04-08 22:36:02 +0100

    Adicionadas especificações. Criado estrutura do grafo e pequeno teste.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct
 {
+	int u, v;
+} edge;
+
+typedef struct graph_element
+{
 	int id;
-} vertice;
+	edge *edge_element;
+	struct graph_element *next_element;
+} graph_element;
 
 int main()
 {
-	printf("Hello World!\n");
+	graph_element *test = malloc(sizeof(graph_element));
+	test->id = 1;
+	test->edge_element = malloc(sizeof(edge));
+	test->edge_element->u = 1;
+	test->edge_element->v = 2;
+
+	printf("%d", test->id);
+	printf("%d", test->edge_element->u);
+	printf("%d\n", test->edge_element->v);
+
 	return 0;
 }
