30c91134b18deed2dad91cb1e085e086e548be74
Nuno Meren <nuno.meren@gmail.com>
2023-04-23 10:32:06 +0100

    Graph movido para uma pasta própria. Makefile alterado para compilar graph na nova pasta Dependências do alvo graph corrigidas.

--- makefile
+++ makefile
@@ -1,20 +1,23 @@
 binaryName = prim
+graphBinaryName = graph
 srcDir = ./src/
+graphSrcDir = $(srcDir)graph/
 buildDir = ./build/
 binDir = ./bin/
 objectFiles = $(buildDir)graph.o
 CCFLAGS = -Wall
  
 all: graph
-	$(CC) $(CCFLAGS) $(srcDir)main.c $(objectFiles) -o $(binDir)$(binaryName)
 
-graph:
-	$(CC) $(CCFLAGS) -c $(srcDir)graph.c -o $(buildDir)graph.o
+graph: graph.o
+	$(CC) $(CCFLAGS) $(graphSrcDir)main.c $(objectFiles) -o $(binDir)$(graphBinaryName)
+
+graph.o: $(graphSrcDir)graph.c $(graphSrcDir)graph.h
+	$(CC) $(CCFLAGS) -c $(graphSrcDir)graph.c -o $(buildDir)graph.o
 
 debug: CCFLAGS = -DDEBUG -g
 debug: clean all
 
 clean:
 	rm -f $(buildDir)*.o
-	rm -f $(binDir)*.gch
-	rm -f $(binDir)$(binaryName)
+	rm -f $(binDir)*.*
---
+++ src/graph/main.c
@@ -0,0 +1,60 @@
+/********************************************************************
+ *  Projecto Final em Engenharia Informática                        *
+ *                                                                  *
+ * Programação Paralela Multi tarefa								*
+ * Algoritmo de Prim para obter uma árvore geradora mínima			*
+ * 																	*
+ * Gerador de grafos												*
+ * 																	*
+ * Nuno Méren														*
+ * 1902937															*
+ * 																	*
+ * Orientador: Paulo Shirley										*
+ * ******************************************************************/
+
+#include <stdio.h>
+#include <time.h>
+
+#ifndef GRAPH
+#define GRAPH
+#include "graph.h"
+#endif
+
+int main()
+{
+	srand(time NULL);
+
+	graph *test = create_graph();
+
+	// impressão do grafo de teste
+	while (test->edge_head != NULL)
+	{
+		printf("aresta: %d->%d (%d)\n", test->edge_head->u, test->edge_head->v, test->edge_head->weight);
+		test->edge_head = test->edge_head->next_edge;
+	}
+
+	while (test->vertice_head != NULL)
+	{
+		printf("vertice: %d\n", test->vertice_head->id);
+		test->vertice_head = test->vertice_head->next_vertice;
+	}
+
+	// limpeza da memória
+	while (test->vertice_head != NULL)
+	{
+		vertice *temp = test->vertice_head->next_vertice;
+		free(test->vertice_head);
+		test->vertice_head = temp;
+	}
+
+	while (test->edge_head != NULL)
+	{
+		edge *temp = test->edge_head->next_edge;
+		free(test->edge_head);
+		test->edge_head = temp;
+	}
+
+	free(test);
+
+	return 0;
+}
