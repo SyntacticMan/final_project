50d421ffd8468f28c4c1f1af083653743edb8718
Nuno Meren <nuno.meren@gmail.com>
2023-04-24 20:47:57 +0100

    Grafo agora é guardado com matriz de adjacência. Grafo gerado é guardado como uma matriz de adjacência triangular superior. lista de tarefas actualizada.

--- src/graph/graph.c
+++ src/graph/graph.c
@@ -1,7 +1,10 @@
+#ifndef GRAPH
+#define GRAPH
 #include "graph.h"
-
+#endif
 /*
- *   Função random_generator
+ *   random_generator
+
  *   gera um inteiro entre min e max
  */
 int random_generator(int max, int min)
@@ -10,24 +13,26 @@ int random_generator(int max, int min)
 }
 
 /*
- * Função create_graph
+ * create_graph
+
  * cria um novo grafo, com lista de vértices e arestas
  */
 int **create_graph(int graph_size)
 {
     // alocar as colunas
-    int **new_graph = (int **)malloc((graph_size + (graph_size - 1)) * sizeof(int *));
+    int **new_graph = (int **)malloc(get_array_size(graph_size) * sizeof(int *));
 
+    printf("Array size: %d\n", get_array_size(graph_size));
     // como a matriz é triangular superior
     // apenas aloco os elementos que vão ser utilizados
-    for (int col = 0; col <= graph_size; col++)
+    for (int col = 1; col <= graph_size; col++)
     {
-        for (int row = 0; row < graph_size; row++)
+        for (int row = 1; row < graph_size; row++)
         {
             // se linha = coluna o vértice ligar-se-ia a ele mesmo
             // se linha > coluna estou na triangular inferior
             // em ambos os casos passo à frente
-            if ((row == col) || (row > col))
+            if ((row >= col))
                 continue;
 
             add_edge(new_graph, col, row);
@@ -37,79 +42,58 @@ int **create_graph(int graph_size)
     return new_graph;
 }
 
-int get_col_index(int col)
+/*
+    get_index
+
+    converte a coluna e linha no indíce correspondente
+    no vetor que guarda o grafo
+*/
+int get_index(int col, int row)
 {
-    if (col == 2)
+    // o array apenas guarda colunas a partir da 2
+    if (col <= 2)
         return 0;
 
     int index = 0;
 
-    for (int i = 2; i < col; i++)
+    // obter o indíce da coluna
+    for (int i = 0; i < col; i++)
     {
         index += i - 1;
     }
 
+    // adicionar a linha ao indíce
+    // dá o indíce da coluna/linha
+    index += row;
+
     return index;
 }
 
-int get_row_index(int col_index, int row)
+/*
+    get_array_size
+
+    calcula o tamanho necessário que o array tem de ter
+    obtendo com base no indíce da última coluna
+*/
+int get_array_size(int graph_size)
 {
-    return 0;
+    return get_index(graph_size, 0) + (graph_size - 1);
 }
 
 /*
- * Função add_edge
+ * add_edge
+
  * cria uma aresta entre os vértices u e v
  */
 void add_edge(int **graph, int u, int v)
 {
-    int col_index = get_col_index(u);
-    int row_index = col_index + v;
+    int index = get_index(u, v);
 
     // o peso é atribuído ao acaso
-    int weight = random_generator(MAX_WEIGHT, MIN_WEIGHT);
+    int *weight = (int *)malloc(sizeof(int));
+    *weight = random_generator(MAX_WEIGHT, MIN_WEIGHT);
 
-    printf("col: %d, row: %d, weight: %d\n", u, v, weight);
-    graph[row_index] = (int *)malloc(sizeof(int));
-    graph[row_index] = &weight;
+    printf("(%d,%d) -> row_index= %d, weight: %d\n", u, v, index, *weight);
+    // graph[row_index] = (int *)malloc(sizeof(int));
+    graph[index] = weight;
 }
-
-// /*
-//  * Função check_edge
-//  * devolve o número de arestas que o vertice dado
-//  * tem na lista de arestas edge
-//  */
-// int check_edge(edge *edge_head, int vertice)
-// {
-//     // se ainda não tiverem sido criadas arestas
-//     // devolver logo contagem a 0
-//     if (edge_head == NULL)
-//         return 0;
-
-//     edge *edge_list = edge_head;
-//     int vertice_count = 0;
-
-//     while (edge_list != NULL)
-//     {
-//         if (edge_list->u == vertice || edge_list->v == vertice)
-//             vertice_count++;
-
-//         edge_list = edge_list->next_edge;
-//     }
-
-//     return vertice_count;
-// }
-
-// /*
-//  * Função add_vertice
-//  * adiciona um vértice com id n
-//  */
-// vertice *add_vertice(int n)
-// {
-//     vertice *new_vertice = malloc(sizeof(vertice));
-
-//     new_vertice->next_vertice = NULL;
-//     new_vertice->id = n;
-
-//     return new_vertice;
-// }
