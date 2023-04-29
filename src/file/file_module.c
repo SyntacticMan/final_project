f11865d58e8a6ede462119c86e67405905bf36a0
Nuno Meren <nuno.meren@gmail.com>
2023-04-29 20:43:05 +0100

    write_file agora remove o ficheiro a gravar se já existir.

--- src/file/file_module.c
+++ src/file/file_module.c
@@ -5,16 +5,28 @@
 
 void write_file(header *file_header, int **graph, char *filename)
 {
-    FILE *graph_file = fopen(filename, "ab");
+    // abrir o ficheiro em modo de escrita binária
+    // assim certifico-me que, se ele já existir, é apagado
+    FILE *graph_file = fopen(filename, "wb");
 
     if (!graph_file)
     {
-        printf("Nao foi possivel abrir o ficheiro.\n");
+        printf("Nao foi possivel abrir %s.\n", filename);
         return;
     }
 
     // guardar o tamanho no cabeçalho e escrevê-lo no ficheiro
     fwrite(file_header, sizeof(header), 1, graph_file);
+
+    // para a adição do grafo é necessário reabrir o ficheiro
+    // em modo de apensar para não destruir o cabeçalho
+    graph_file = freopen(filename, "ab", graph_file);
+
+    if (graph_file == NULL)
+    {
+        printf("Nao foi possivel abrir %s.\n", filename);
+        return;
+    }
 
     for (int i = 0; i < file_header->array_size; i++)
     {
