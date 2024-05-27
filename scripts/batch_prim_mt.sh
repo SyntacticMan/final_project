3fed78a36b6f394691ddd113cddf9844e0a135c4
Nuno Meren <nuno.meren@gmail.com>
2024-05-27 08:04:29 +0100

    adicionado script para processar os grafos em tarefa múltipla

---
+++ scripts/batch_prim_mt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Pasta contendo os arquivos .grf
+input_directory="graphs"
+
+# Verifica se a pasta existe
+if [ ! -d "$input_directory" ]; then
+    echo "Pasta $input_directory não encontrada."
+    exit 1
+fi
+
+# Loop para percorrer todos os arquivos .grf na pasta
+for file in "$input_directory"/*.grf; do
+    # Verifica se o arquivo é um arquivo .grf
+    if [ -f "$file" ]; then
+        echo "Executando comando para $file:"
+        ./bin/prim -f "$file" -t 3
+        echo "-----------------------------------"
+    fi
+done
