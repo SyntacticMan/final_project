05098445302f36c662566b956627d3459d5a512b
SyntacticMan <nuno.meren@gmail.com>
2024-06-01 20:24:18 +0100

    script alterado para receber a variação do tamanho dos grafos criados como um parâmetro

--- scripts/batch_graph_generate.sh
+++ scripts/batch_graph_generate.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 # Definição das opções padrão do comando
-initial_size=6
 prefix="graph"
 extension=".grf"
-percentage=40
+percentage=45
+size=10
 
 # Número de grafos padrão
 default_num_graphs=5
+default_size= 10
 
 # Função para exibir o uso do script
 usage() {
-    echo "Uso: $0 [-n <num_graphs>]"
+    echo "Uso: $0 [-n <num_graphs>] [-s <size_increment>]"
     echo "   -n <num_graphs>: Número de grafos a serem gerados (padrão: $default_num_graphs)"
+    echo "   -s <size_increment>: Tamanho dos grafos a serem gerados (padrão: $default_size)"
     exit 1
 }
 
 # Parsing dos argumentos usando getopts
-while getopts ":n:" opt; do
+while getopts ":n:s:" opt; do
     case ${opt} in
         n )
             num_graphs=$OPTARG
+            ;;
+        s)
+            size_increment=$OPTARG
             ;;
         \? )
             echo "Opção inválida: -$OPTARG" 1>&2
@@ -35,9 +40,7 @@ done
 
 # Definindo o número de grafos como o padrão se não fornecido
 num_graphs=${num_graphs:-$default_num_graphs}
-
-# Incremento do tamanho do grafo a cada iteração
-size_increment=2
+size_increment=${size_increment:-$default_size}
 
 # Diretório para armazenar os grafos
 output_directory="graphs"
@@ -45,18 +48,14 @@ output_directory="graphs"
 # Cria o diretório se ele não existir
 mkdir -p "$output_directory"
 
-# Inicialize o tamanho com o tamanho inicial
-size=$initial_size
-
 # Loop para gerar múltiplos grafos
 for ((i=1; i<=num_graphs; i++)); do
-    # Construindo o nome do arquivo de saída
+
     output_file="$output_directory/${prefix}_s${size}_p${percentage}${extension}"
 
-    echo "Geração do Grafo $i:"
+    echo "Geração do Grafo $i de $num_graphs:"
     ./bin/gengraph -s $size -f $output_file -p $percentage
     echo "-----------------------------------"
 
-    # Aumentando o tamanho para a próxima iteração
     size=$((size + size_increment))
 done
