2de4465b365ab9e7f51f0941da3fca15e7d84daa
Nuno Méren <nuno.meren@gmail.com>
2024-06-03 14:12:44 +0100

    script de geração alterado para gerar grafos somando 500 vértices em cada iteração e para as percentagens 25, 50 e 75

--- scripts/batch_graph_generate.sh
+++ scripts/batch_graph_generate.sh
@@ -3,44 +3,6 @@
 # Definição das opções padrão do comando
 prefix="graph"
 extension=".grf"
-percentage=45
-size=10
-
-# Número de grafos padrão
-default_num_graphs=5
-default_size= 10
-
-# Função para exibir o uso do script
-usage() {
-    echo "Uso: $0 [-n <num_graphs>] [-s <size_increment>]"
-    echo "   -n <num_graphs>: Número de grafos a serem gerados (padrão: $default_num_graphs)"
-    echo "   -s <size_increment>: Tamanho dos grafos a serem gerados (padrão: $default_size)"
-    exit 1
-}
-
-# Parsing dos argumentos usando getopts
-while getopts ":n:s:" opt; do
-    case ${opt} in
-        n )
-            num_graphs=$OPTARG
-            ;;
-        s)
-            size_increment=$OPTARG
-            ;;
-        \? )
-            echo "Opção inválida: -$OPTARG" 1>&2
-            usage
-            ;;
-        : )
-            echo "Opção -$OPTARG requer um argumento." 1>&2
-            usage
-            ;;
-    esac
-done
-
-# Definindo o número de grafos como o padrão se não fornecido
-num_graphs=${num_graphs:-$default_num_graphs}
-size_increment=${size_increment:-$default_size}
 
 # Diretório para armazenar os grafos
 output_directory="graphs"
@@ -48,14 +10,19 @@ output_directory="graphs"
 # Cria o diretório se ele não existir
 mkdir -p "$output_directory"
 
+size=500
 # Loop para gerar múltiplos grafos
-for ((i=1; i<=num_graphs; i++)); do
+for ((i=1; i<=3; i++)); do
+	percentage=25
+	for ((j=1; j<=3; j++)); do
+		
+	    output_file="$output_directory/${prefix}_s${size}_p${percentage}${extension}"
 
-    output_file="$output_directory/${prefix}_s${size}_p${percentage}${extension}"
+	    echo "Geração do Grafo $i de 250:"
+	    ./bin/gengraph -s $size -f $output_file -p $percentage
+	    echo "-----------------------------------"
 
-    echo "Geração do Grafo $i de $num_graphs:"
-    ./bin/gengraph -s $size -f $output_file -p $percentage
-    echo "-----------------------------------"
-
-    size=$((size + size_increment))
+	    size=$((size + 500))
+	    percentage=$((percentage + 25))
+    done
 done
